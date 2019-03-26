3dc78d6fe365258e1b4951b9ec89d64a3c13d2ef
Gonzalo Rodríguez Ortega <gonrodri11@yahoo.es>
2019-03-26 22:31:28 +0100

    Ej 9 de datos simples corregido

--- Datos simples/Ejercicio9.py
+++ Datos simples/Ejercicio9.py
@@ -1,6 +1,8 @@
+#Escribir un programa que pregunte al usuario una cantidad a invertir, el interés porcentual anual y el número de años, y muestre por pantalla el capital obtenido en la inversión redondeado con dos decimales.
+
+
 c = float(input('cantidad a invertir:'))
 i = float (input('interés anual:'))
 y = int(input('Numero de años:'))
 print ('Capital final:' + str (round (c * (i/100 + 1) ** y,2 )))
 
-#no entiendo
